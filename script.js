accc89c458e5e34a9c3865c77336c32b20c39d93
padraic <high5developer@gmail.com>
2020-03-12 10:41:36 +0000

    added lesson sections

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
 const grammarPoint = document.getElementById("grammar-point");
-// const level = document.getElementById("level");
 const question = document.getElementById("question");
 const answer = document.getElementById("answer");
 const addGrammarPointBtn = document.getElementById("submit-btn");
-const classLevel = document.getElementById("class-level");
+const classLevel = document.getElementById("grammar-point-level");
 const classGrammarPoint = document.getElementById("class-grammar-point");
 const list = document.getElementById("queslist");
-const addPhrasalVerbBtn = document.getElementById('add-phrasal-verb-btn');
-const pvLevel = document.getElementById('pv-level');
-const phrasalVerb = document.getElementById('phrasal-verb');
-const pvExample = document.getElementById('pv-example');
-const pvTense = document.getElementById('pv-tense');
-const pvQuestion = document.getElementById('pv-question');
-const lessonLevel = document.getElementById('lesson-level');
-const generateLessonBtn = document.getElementById('generate-lesson-btn');
-const lessonGP = document.getElementById('gp-lesson');
-const lessonPV = document.getElementById('pv-question');
+const addPhrasalVerbBtn = document.getElementById("add-phrasal-verb-btn");
+const pvLevel = document.getElementById("pv-level");
+const phrasalVerb = document.getElementById("phrasal-verb");
+const pvExample = document.getElementById("pv-example");
+const pvTense = document.getElementById("pv-tense");
+const pvQuestion = document.getElementById("pv-question");
+const lessonLevel = document.getElementById("lesson-level");
+const generateLessonBtn = document.getElementById("generate-lesson-btn");
+const lessonGP = document.getElementById("gp-lesson");
+const lessonPV = document.getElementById("pv-lesson");
 
 
 
-
-
-
-let gpDB = [{
-    name: 'poi'
-}];
-
-export default function send() {
-    console.log('send function');
-}
-
-
+// Databases
+let gpDB = [];
 let phrasalVerbDB = [];
+let vocabDB = [];
+let verbDB = [];
+let miniPresDB = [];
+let transDB = [];
+let picDB = [];
+let pronDB = [];
+let articleDB = [];
+let expressionDB = [];
 
 // add Grammar Point function
 function addGrammarPoint(e) {
@@ -50,7 +47,6 @@ function addGrammarPoint(e) {
     };
     gpDB.push(newGrammarPoint);
     console.log(gpDB);
-
 }
 
 // add Phrasal Verb Fundtion
@@ -63,7 +59,7 @@ function addPhrasalVerb(e) {
         example: pvExample.value,
         question: pvQuestion.value,
         tense: pvTense.value
-    }
+    };
     phrasalVerbDB.push(newPhrasalVerb);
     console.log(phrasalVerbDB);
 }
@@ -92,37 +88,31 @@ function generateLesson(e) {
 
     phrasalVerbDB.forEach(item => {
         if (item.level == level) {
+            console.log("pv function");
 
-            const pvEl = document.createElement('p');
+            const pvEl = document.createElement("p");
 
-            pvEl.innerHTML = phrasalVerbDB[0].question;
+            pvEl.innerHTML = item.question;
 
-
+            console.log(lessonPV);
             lessonPV.appendChild(pvEl);
-
         }
     });
 
-
     gpDB.forEach(item => {
         if (item.level == level) {
-            const gpEl = document.createElement('p');
+            const gpEl = document.createElement("p");
 
             gpEl.innerHTML = item.ques;
 
             lessonGP.appendChild(gpEl);
-
-
-
+            console.log(lessonGP);
         }
     });
-
-
 }
 
-
 //Event Listeners
-addPhrasalVerbBtn.addEventListener('click', addPhrasalVerb);
+addPhrasalVerbBtn.addEventListener("click", addPhrasalVerb);
 addGrammarPointBtn.addEventListener("click", addGrammarPoint);
 classLevel.addEventListener("change", lesson);
-generateLessonBtn.addEventListener('click', generateLesson);
+generateLessonBtn.addEventListener("click", generateLesson);
